7d7cc8749d2a944f920b967cd80b939f169f3860
jwathika <wathika02@gmail.com>
2024-04-27 13:27:23 +0300

    Add EyesOnMe and Keypad components

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
+import { useState } from 'react';
+
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+	const [mouse, setMouse] = useState(false);
+	function focusedEvent() {
+		setMouse(console.log('Good!'));
+	}
+	function blurredEvent() {
+		setMouse(console.log('Hey! Eyes on me!'));
+	}
+	return (
+		<>
+			<button onFocus={focusedEvent} onBlur={blurredEvent}>
+				Eyes on me
+			</button>
+		</>
+	);
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import { useState } from 'react';
+
+function Keypad() {
+	const [input, setInput] = useState(false);
+
+	function handler(event) {
+		setInput(console.log('Entering password...'));
+	}
+	return (
+		<div>
+			<input type='password' onChange={handler}></input>
+		</div>
+	);
 }
 
 export default Keypad;
